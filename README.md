22d5f61a01514f51724d89b4c3b262a9d814b790
Scott Turnbull <scott.turnbull@henrico.us>
2015-05-14 16:57:23 -0400

    Added tabs and such.

--- README.md
+++ README.md
@@ -3,6 +3,20 @@ Overview
 
 Not much to see here. This is just some code I'm poking at as I work through some AngularJS and NodeJS tutorials for fun.
 
+Dependencies
+------------
+
+The only dependencies are for running this under a nodejs http server for development which requires NodeJS (of course). The rest is setup through the package.json and bower.json files.
+
+Running Development Server
+--------------------------
+
+To run the NodeJS Web server just go to a command line and type
+
+```
+  npm start
+```
+
 Resources
 ---------
 
--- assets/js/app.js
+++ assets/js/app.js
@@ -5,20 +5,56 @@
     this.products = gems;
   });
 
+  app.controller('PanelController', function(){
+    this.tab = 1;
+    this.selectedTab = function(setTab) {
+      this.tab = setTab;
+    }
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    }
+  });
+
   var gems = [
     {
       name: 'Icosohedron',
-      price: 3.74,
+      price: 20.74,
       description: 'a solid figure with twenty plane faces, especially equilateral triangular ones.',
       canPurchase: true,
       isSoldOut: false,
+      img: "http://icons.iconarchive.com/icons/iconcubic/dnd-dice/128/d20-icon.png",
     },
     {
       name: 'Cube',
-      price: 2.75,
+      price: 6.75,
       description: 'a six sided 3 dimensional square.',
       canPurchase: true,
       isSoldOut: false,
+      img: "http://icons.iconarchive.com/icons/iconcubic/dnd-dice/128/d6-icon.png",
+    },
+    {
+      name: 'Tetrahedron',
+      price: 4,
+      description: 'a four sided pyramid looking thing.',
+      canPurchase: true,
+      isSoldOut: false,
+      img: "http://icons.iconarchive.com/icons/iconcubic/dnd-dice/128/d4-icon.png",
+    },
+    {
+      name: 'Octohedron',
+      price: 8.23,
+      description: 'an eight sided die.',
+      canPurchase: true,
+      isSoldOut: false,
+      img: "",
     }
   ];
+
+  app.filter('capitalize', function() {
+    return function(input, scope) {
+      if (input!=null)
+      input = input.toLowerCase();
+      return input.substring(0,1).toUpperCase()+input.substring(1);
+    }
+  });
 })();
