7539c9a0d8a172907d5d3f99298c7e2f1bf41e2e
Scott Turnbull <scott.turnbull@henrico.us>
2015-05-15 14:50:09 -0400

    Finished section 3 of Code School

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,11 @@
-(function(){
+(function() {
   var app = angular.module('gemStore', []);
 
-  app.controller('StoreController', function(){
+  app.controller('StoreController', function() {
     this.products = gems;
   });
 
-  app.controller('PanelController', function(){
+  app.controller('PanelController', function() {
     this.tab = 1;
     this.selectedTab = function(setTab) {
       this.tab = setTab;
@@ -15,46 +15,64 @@
     }
   });
 
-  var gems = [
-    {
-      name: 'Icosohedron',
-      price: 20.74,
-      description: 'a solid figure with twenty plane faces, especially equilateral triangular ones.',
-      canPurchase: true,
-      isSoldOut: false,
-      img: "http://icons.iconarchive.com/icons/iconcubic/dnd-dice/128/d20-icon.png",
-    },
-    {
-      name: 'Cube',
-      price: 6.75,
-      description: 'a six sided 3 dimensional square.',
-      canPurchase: true,
-      isSoldOut: false,
-      img: "http://icons.iconarchive.com/icons/iconcubic/dnd-dice/128/d6-icon.png",
-    },
-    {
-      name: 'Tetrahedron',
-      price: 4,
-      description: 'a four sided pyramid looking thing.',
-      canPurchase: true,
-      isSoldOut: false,
-      img: "http://icons.iconarchive.com/icons/iconcubic/dnd-dice/128/d4-icon.png",
-    },
-    {
-      name: 'Octohedron',
-      price: 8.23,
-      description: 'an eight sided die.',
-      canPurchase: true,
-      isSoldOut: false,
-      img: "",
+  app.controller('ReviewController', function() {
+    this.review = {};
+
+    this.addReview = function(gem) {
+      this.review.createdOn = Date.now();
+      gem.reviews.push(this.review);
+      this.review = {};
     }
-  ];
+  });
+
+  var gems = [{
+    name: 'Icosohedron',
+    sides: 20,
+    price: 20.3,
+    description: 'a solid figure with twenty plane faces, especially equilateral triangular ones.',
+    canPurchase: true,
+    isSoldOut: false,
+    img: "http://icons.iconarchive.com/icons/iconcubic/dnd-dice/128/d20-icon.png",
+    reviews: [{
+      stars: 5,
+      body: "I'm a D&D player and this is the only die you need!",
+      author: "gary@dnd.com",
+    }, {
+      stars: 1,
+      body: "GURPS players don't need no stinkin d20",
+      author: "steve@jacksongames.com",
+    }, ],
+  }, {
+    name: 'Cube',
+    price: 6.75,
+    description: 'a six sided 3 dimensional square.',
+    canPurchase: true,
+    isSoldOut: false,
+    img: "http://icons.iconarchive.com/icons/iconcubic/dnd-dice/128/d6-icon.png",
+    reviews: [],
+  }, {
+    name: 'Tetrahedron',
+    price: 4,
+    description: 'a four sided pyramid looking thing.',
+    canPurchase: true,
+    isSoldOut: false,
+    img: "http://icons.iconarchive.com/icons/iconcubic/dnd-dice/128/d4-icon.png",
+    reviews: [],
+  }, {
+    name: 'Octohedron',
+    price: 8.23,
+    description: 'an eight sided die.',
+    canPurchase: true,
+    isSoldOut: false,
+    img: "",
+    reviews: [],
+  }];
 
   app.filter('capitalize', function() {
     return function(input, scope) {
-      if (input!=null)
-      input = input.toLowerCase();
-      return input.substring(0,1).toUpperCase()+input.substring(1);
+      if (input != null)
+        input = input.toLowerCase();
+      return input.substring(0, 1).toUpperCase() + input.substring(1);
     }
   });
 })();
